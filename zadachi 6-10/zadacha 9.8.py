8b083a4c6f094ca385c137b3b42f2bae255e064e
Masster <nikolaysedovv@gmail.com>
2022-09-16 01:55:05 -0700

    9.8 не дописал еще, но скоро допишу, обещаю

--- zadachi 6-10/zadacha 9.8.py
+++ zadachi 6-10/zadacha 9.8.py
@@ -1,5 +1,4 @@
 import random
-import numpy
 counter = 0
 bolshe = []
 m = random.randint(1, 10)
@@ -12,13 +11,8 @@ for i in range(len(matrix)):
     for j in range(len(matrix[i])):
         print(matrix[i][j], end = ' ')
     print()
-# for i in range(len(matrix)):
-#     t = []
-#     count = 0
-#     for j in range(len(matrix)):
-#         t.append(matrix[count][i])
-#     count+=1
-#     print(t)
+
 
 t = list(map(list,zip(*matrix)))
 print(t)
+for i in range(len(t)):
