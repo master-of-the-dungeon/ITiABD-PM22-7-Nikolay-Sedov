0f699398750b0adf1bb654bf135bfab007a4134d
nikolaysedoff <nikolaysedovv@gmail.com>
2022-09-16 11:41:53 +0300

    Решенные задачи 8,9,10

--- 08.09.22/zadacha 9.8.py
+++ 08.09.22/zadacha 9.8.py
@@ -1,4 +1,7 @@
 import random
+import numpy
+counter = 0
+bolshe = []
 m = random.randint(1, 10)
 n = random.randint(1, 10)
 matrix = [[0]*m for i in range(n)]
@@ -9,3 +12,13 @@ for i in range(len(matrix)):
     for j in range(len(matrix[i])):
         print(matrix[i][j], end = ' ')
     print()
+# for i in range(len(matrix)):
+#     t = []
+#     count = 0
+#     for j in range(len(matrix)):
+#         t.append(matrix[count][i])
+#     count+=1
+#     print(t)
+
+t = list(map(list,zip(*matrix)))
+print(t)
