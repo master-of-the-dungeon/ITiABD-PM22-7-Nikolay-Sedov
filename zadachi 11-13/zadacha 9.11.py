c5ac5f906f6e5a8f6b05953afa45b50bf2f553f2
nikolaysedoff <nikolaysedovv@gmail.com>
2022-09-21 18:04:35 +0300

    Начал делать задания 11-20

---
+++ zadachi 11-13/zadacha 9.11.py
@@ -0,0 +1,14 @@
+import random
+d = {a: a**2 for a in range(7)}
+students = {}
+def a(d):
+    for i in range(len(d)):
+        print(d[i])
+def b(d):
+    sum = 0
+    for i in range(len(d)):
+        sum+=d[i]
+    print(sum)
+def c():
+    students = {'Лев Троцкий':[193,79], 'Ганнибал Лектер':[175,98],'Вася Пупкин':[297,130]}
+c()
