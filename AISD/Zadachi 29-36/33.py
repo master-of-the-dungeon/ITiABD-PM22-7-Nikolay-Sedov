e9efb56a576cee88e4a5050028361fb267e58c2a
Nikolay Sedoff <nikolaysedovv@gmail.com>
2023-05-04 11:43:59 +0300

    большой комит

--- AISD/Zadachi 29-36/33.py
+++ AISD/Zadachi 29-36/33.py
@@ -1,57 +1,53 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, val):
+        self.val = val
         self.prev = None
         self.next = None
 
-
-def remove_large_elements(head, max_value):
-    if head is None:
+def delete_greater_than_value(head: Node, val: int) -> Node:
+    # Если список пуст, возвращаем None
+    if not head:
         return None
 
-    current_node = head
-    while True:
-        if current_node.value > max_value:
-            if current_node == head:
-                head = current_node.next
-            current_node.prev.next = current_node.next
-            current_node.next.prev = current_node.prev
-        current_node = current_node.next
-        if current_node == head:
-            break
+    # Находим первый элемент, который не больше заданного значения
+    current = head
+    while current.val > val:
+        current = current.next
+        if current == head:
+            return head
+    head = current
+
+    # Проходим по списку и удаляем элементы, которые больше заданного значения
+    current = head.next
+    while current != head:
+        if current.val > val:
+            # Обновляем ссылки на следующий и предыдущий узлы, чтобы исключить элемент из списка
+            current.prev.next = current.next
+            current.next.prev = current.prev
+            current = current.next
+            continue
+        current = current.next
 
     return head
 
-
+# Создаем кольцевой двусвязный список
 head = Node(1)
-head.prev = Node(8)
-head.prev.next = head
-head.next = Node(5)
+head.next = Node(3)
 head.next.prev = head
-head.next.next = Node(2)
+head.next.next = Node(5)
 head.next.next.prev = head.next
 head.next.next.next = Node(7)
 head.next.next.next.prev = head.next.next
 head.next.next.next.next = head
-head.prev.next = head
+head.prev = head.next.next.next
 
+# Удаляем элементы, которые больше 4
+head = delete_greater_than_value(head, 4)
 
-print("Original List:")
-current_node = head
+# Выводим список
+current = head
 while True:
-    print(current_node.value, end=' ')
-    current_node = current_node.next
-    if current_node == head:
-        break
-
-
-head = remove_large_elements(head, 5)
-
-
-print("\nModified List:")
-current_node = head
-while True:
-    print(current_node.value, end=' ')
-    current_node = current_node.next
-    if current_node == head:
+    print(current.val, end=' ')
+    current = current.next
+    if current == head:
         break
